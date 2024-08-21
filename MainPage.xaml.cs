9bbd435baf2d50d3975efb96cdad2497d9e94b0f
User <User@B20>
2024-08-21 13:57:05 +0800

    Add project files.

---
+++ MainPage.xaml.cs
@@ -0,0 +1,70 @@
+﻿namespace Module01Exercise01
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        public void OnSubmitClicked(object sender, EventArgs e)
+        {
+            this.Resources["DynamicSubmissionMessage"] = "Submission Success";
+        }
+
+        public void OnClickedDarkMode(object sender, EventArgs e)
+        {
+            this.Resources["LocalBackgroundColor"] = Colors.Gray;
+            this.Resources["DynamicSubmitButton"] = Colors.Black;
+            this.Resources["DynamicSubmitButtonText"] = Colors.White;
+        }
+
+        public void OnClickedLightMode(object sender, EventArgs e)
+        {
+            this.Resources["LocalBackgroundColor"] = Colors.Teal;
+            this.Resources["DynamicSubmitButton"] = Colors.White;
+            this.Resources["DynamicSubmitButtonText"] = Colors.Black;
+        }
+
+        private void OnNameEntryTextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (!string.IsNullOrEmpty(e.NewTextValue))
+            {
+                DynamicNameEntry.Text = e.NewTextValue;
+            }
+            else
+            {
+                DynamicNameEntry.Text = (string)this.Resources["DynamicName"];
+            }
+        }
+
+        private void OnEmailEntryTextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (!string.IsNullOrEmpty(e.NewTextValue))
+            {
+                DynamicEmailEntry.Text = e.NewTextValue;
+            }
+            else
+            {
+                DynamicEmailEntry.Text = (string)this.Resources["DynamicEmail"];
+            }
+        }
+
+        private void OnBioEntryTextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (!string.IsNullOrEmpty(e.NewTextValue))
+            {
+                DynamicBioEntry.Text = e.NewTextValue;
+            }
+            else
+            {
+                DynamicBioEntry.Text = (string)this.Resources["DynamicBio"];
+            }
+        }
+
+        private void OnDatePickerDateSelected(object sender, DateChangedEventArgs e)
+        {
+            DynamicDateOfBirthEntry.Text = $"Date of Birth: {e.NewDate:MM/dd/yyyy}";
+        }
+    }
+}
---
+++ WelcomeMessage.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module01Exercise01
+{
+    internal class WelcomeMessage : IMarkupExtension
+    {
+        public object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return "Welcome! Today is: " + DateTime.Now.ToString("D");
+        }
+    }
+}
